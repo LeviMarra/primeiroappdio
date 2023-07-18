0b2d2502db906d55667831b3995fdabb66c1a6c1
Leví Marra <levilopesmm@hotmail.com>
2023-07-18 16:33:11 -0300

    Commit de desafio alunos DIO do curso Entendendo a Estrutura e Depurando Apps Androis

---
+++ app/src/main/java/com/example/primeiroappdio/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.primeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // O DESAFIO SERÁ CRIAR UM VALOR DENTRO DO STRING.XML
+        // E TROCAR O TEXTO DO XML E TORNAR INTERNACIONAL (INGLES, ESPANHOL, ETC...)
+
+    }
+
+}
